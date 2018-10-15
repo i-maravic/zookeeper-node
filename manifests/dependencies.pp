c5019aa59747ab126430669d7647ae28916b6be4
Krzysztof Krzyżaniak (eloy) <kkrzyzaniak@egnyte.com>
2018-10-15 11:38:26 +0200

    replace with selector

--- manifests/dependencies.pp
+++ manifests/dependencies.pp
@@ -1,10 +1,15 @@
 class zookeeper_node::dependencies (
-  $provider = 'gem',
 ) {
+
+  $provider = versioncmp($::puppetversion, '4') ? {
+    1       => 'puppet_gem',
+    default => 'gem'
+  }
+
   package { 'zookeeper-gem':
     ensure   => 'latest',
     name     => 'zookeeper',
-    provider => $::zookeeper_node::dependencies::provider,
+    provider => $provider,
   }
 
   Package['zookeeper-gem'] -> Zookeeper_node_t <| |>
